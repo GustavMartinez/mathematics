985cc9bc9cc04f90d8cc0f2f212098371c560df4
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2026-04-06 13:58:12 -0300

    Aritmetica - resta de enteros

--- Aritmetica/02_resta_de_enteros.py
+++ Aritmetica/02_resta_de_enteros.py
@@ -1,3 +1,28 @@
 # RESTA
 
 # Es la operación inversa de la suma o adición. Los elementos de una resta son el minuendo (+), sustraendo (−) y la diferencia.
+
+#   5  <<< Minuendo
+#  -2  <<< Sustraendo
+# -----
+#   3  <<< Diferencia
+
+
+# Resta de dos números enteros
+x = 10
+y = 2
+
+print(f"La resta de {x} y {y} es: {x-2}")
+
+# Resta de dos números enteros
+
+x = 854
+y = 12
+print(f"La resta de {x} y {y} es: {x-y}")
+
+# Resta de tres números enteros
+x = 25
+y = -685
+z = 426
+
+print(f"La resta de: {x}, {y} y {z}; ({x}-({y})-{z}) es: {x-y-z}")
