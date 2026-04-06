371cd69f46c0f423c6fc763b1abdf4086c59716e
Gustavo <gustavo.a.t.m.12345@hotmail.com>
2026-04-06 13:42:29 -0300

    Aritmetica - suma de números enteros

---
+++ Aritmetica/01_suma_de_enteros.py
@@ -0,0 +1,16 @@
+# SUMA
+
+# suma de dos números enteros
+x = 364
+y = 93
+print(f"La suma de {x} y {y} es: {x+y}")
+
+# suma de tres números enteros
+x = 4050
+y = 2019
+z = 310
+print(f"La suma de {x}, {y}, y {z} es: {x+y+z}")
+
+# suma de cuatro números enteros
+x = 1995 + 12 + 3 + 9
+print(x)
