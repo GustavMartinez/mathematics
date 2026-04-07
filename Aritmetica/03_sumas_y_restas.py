838eb29e4590685e2333738260d7a53a414b470a
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2026-04-07 02:11:46 -0300

    suma y resta con agrupacion

--- Aritmetica/03_sumas_y_restas.py
+++ Aritmetica/03_sumas_y_restas.py
@@ -4,4 +4,7 @@ y = 24
 print(f"La suma de {x} y {y}, es: {x+y}")
 print(f"La resta de {x} y {y}, es: {x-y}")
 
+# Suma y resta con agrupación
 
+print(((-8+6)-(-3-2))+(4-(2-1)))
+print((8+5)-(-13+2))
