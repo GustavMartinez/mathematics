bddadd5a0aef831e6e05618d89675b1b1a1a700d
GustavMartinez <gustavo.a.t.m.12345@hotmail.com>
2026-04-07 02:48:24 -0300

    Aritmetica - multiplicacion

---
+++ Aritmetica/04_multiplicacion.py
@@ -0,0 +1,33 @@
+# MULTIPLICACIÓN
+
+# La multiplicación es la representación de la suma de una misma cantidad varias veces. Una multiplicación se representa con: *
+
+# Los elementos de una multiplicación reciben el nombre de factores y, el resultado, producto o multiplicación.
+
+x = 5
+y = 3
+
+print(x*y)
+
+print(358*6)
+
+
+# LEY DE SIGNOS
+
+# (+)(+) = + ; (+)(-) = - ; (-)(-) = +;
+
+print((-3)*(-6)*(-4))
+print((3)*(-5)*(-2)*(4))
+
+
+# En una caja hay 24 refrescos, cuántos refrescos habrá en 9 cajas?
+
+refrescos_x_caja = 24
+num_cajas = 9
+
+print(refrescos_x_caja*num_cajas)
+
+
+# Agrupación:
+
+print(2*(7-4)+3*(1-5)+8)
